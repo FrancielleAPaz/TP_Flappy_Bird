5b34fd735b89df2957b9a86c2095d008c8232a27
Camila Ferreira Santiago <camilaferreira044@gmail.com>
2025-06-25 00:24:17 -0300

    Atualizando documentação Doxygen após mudanças no código

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -6,7 +6,8 @@ var searchData=
   ['scenario_2ehpp_3',['Scenario.hpp',['../_scenario_8hpp.html',1,'']]],
   ['setanimationspeed_4',['setAnimationSpeed',['../class_bird.html#aec25f2e05e3668df8396ba77bb897d1f',1,'Bird']]],
   ['setdifficulty_5',['setDifficulty',['../class_scenario.html#ac13f56cc04ea28f1ab7ea905c31a9c39',1,'Scenario']]],
-  ['setspeed_6',['setSpeed',['../class_pipe.html#aa66127299f6c88adaf82d4a31a79e3cc',1,'Pipe']]],
-  ['showgameover_7',['showGameOver',['../class_game.html#a8e8d1f6410bd0188e106cf795c87b8e5',1,'Game']]],
-  ['startnewround_8',['startNewRound',['../class_game.html#a32a8f6a17e1e95df3f2f7a3b7bc8f99b',1,'Game']]]
+  ['setscored_6',['setScored',['../class_pipe.html#a1f436e349894a1bd6641302facfe6646',1,'Pipe']]],
+  ['setspeed_7',['setSpeed',['../class_pipe.html#aa66127299f6c88adaf82d4a31a79e3cc',1,'Pipe']]],
+  ['showgameover_8',['showGameOver',['../class_game.html#a8e8d1f6410bd0188e106cf795c87b8e5',1,'Game']]],
+  ['startnewround_9',['startNewRound',['../class_game.html#a32a8f6a17e1e95df3f2f7a3b7bc8f99b',1,'Game']]]
 ];
--- docs/html/search/functions_e.js
+++ docs/html/search/functions_e.js
@@ -4,7 +4,8 @@ var searchData=
   ['scenario_1',['Scenario',['../class_scenario.html#ac81a05dad61b30116332ea971359a41e',1,'Scenario']]],
   ['setanimationspeed_2',['setAnimationSpeed',['../class_bird.html#aec25f2e05e3668df8396ba77bb897d1f',1,'Bird']]],
   ['setdifficulty_3',['setDifficulty',['../class_scenario.html#ac13f56cc04ea28f1ab7ea905c31a9c39',1,'Scenario']]],
-  ['setspeed_4',['setSpeed',['../class_pipe.html#aa66127299f6c88adaf82d4a31a79e3cc',1,'Pipe']]],
-  ['showgameover_5',['showGameOver',['../class_game.html#a8e8d1f6410bd0188e106cf795c87b8e5',1,'Game']]],
-  ['startnewround_6',['startNewRound',['../class_game.html#a32a8f6a17e1e95df3f2f7a3b7bc8f99b',1,'Game']]]
+  ['setscored_4',['setScored',['../class_pipe.html#a1f436e349894a1bd6641302facfe6646',1,'Pipe']]],
+  ['setspeed_5',['setSpeed',['../class_pipe.html#aa66127299f6c88adaf82d4a31a79e3cc',1,'Pipe']]],
+  ['showgameover_6',['showGameOver',['../class_game.html#a8e8d1f6410bd0188e106cf795c87b8e5',1,'Game']]],
+  ['startnewround_7',['startNewRound',['../class_game.html#a32a8f6a17e1e95df3f2f7a3b7bc8f99b',1,'Game']]]
 ];
